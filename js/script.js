f7813fdae2c8e0004b1fb77f43370eedd925ea48
filipe <filipe.dias09@hotmail.com>
2022-10-17 12:12:05 -0300

    testes de eventos

--- js/script.js
+++ js/script.js
@@ -5,8 +5,20 @@ botao.onclick = function() {
 }
 */
 
+/*
 // Usando Event Listener
 const botao = document.getElementById("bt-dh") 
 botao.addEventListener("click", function() {
     console.log("43 é a resposta!")
 })
+*/
+
+// Eevento da página carregada
+/*
+window.addEventListener("load", function(){
+    const botao = document.getElementById("bt-dh") 
+botao.addEventListener("click", function() {
+    console.log("43 é a resposta!")
+})
+})
+*/
