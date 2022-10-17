558fb1b5b12cbf89c72273e950e76d375875af72
filipe <filipe.dias09@hotmail.com>
2022-10-17 11:13:30 -0300

    event listener

--- js/script.js
+++ js/script.js
@@ -1,4 +1,12 @@
+/*
 const botao = document.getElementById("bt-dh") 
 botao.onclick = function() {
     console.log("43 é a resposta!")
 }
+*/
+
+// Usando Event Listener
+const botao = document.getElementById("bt-dh") 
+botao.addEventListener("click", function() {
+    console.log("43 é a resposta!")
+})
